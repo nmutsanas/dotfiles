a8371ce36b2199ec6c1c9c732a5eed74d6c2301a
Nikos Mutsanas <nigaki@gmail.com>
2021-05-07 23:17:24 +0200

    better polybar configuration, borg nag script

--- polybar/polybar.configdir.symlink/myscripts/polybar-borg-nag.sh
+++ polybar/polybar.configdir.symlink/myscripts/polybar-borg-nag.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 THRESHHOLD=100
+if [[ ! -f /mnt/data/borgrepo ]]; then
+	exit;
+fi
 LASTBACKUP=$(borg list --format="{start}" --last 1 /mnt/data/borgrepo | awk '{print $2}')
 DAYSSINCE=$(datediff $LASTBACKUP $(date +%F))
 if [[ $DAYSSINCE -ge $THRESHHOLD ]]; then

a8ade11d8bb98f36ec316f077e3774fc6b5f12f0
Nikos Mutsanas <nigaki@gmail.com>
2021-05-07 23:15:06 +0200

    better whatsapp window handling, better backlight adjustment command, remove deprecated completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,9 +1,4 @@
 #!/bin/zsh
-_activate_dt5_db(){
-	_alternative "dbs:available databases:($(list-mysql-dbs))"
-}
-compdef _activate_dt5_db activate_dt5_db
-
 _c() { _files -W ~/code/ -/; }
 compdef _c c
 
